6a6162933d5a91bb8eedde65e38a65071aa593b9
Hannes <hannesro27@gmail.com>
2022-12-14 13:57:51 +0100

    typo in main menu, bug with the json folder location fixed

--- passwortmanager/manager_ui.py
+++ passwortmanager/manager_ui.py
@@ -4,7 +4,7 @@ def show_menu():
     print("===================================================")
     print("                  Passwortmanager")
     print("===================================================")
-    print("  1) Erstelle ein neues Passwort in der Datenbank")
+    print("  1) Erstelle eine Passwort Datenbank")
     print("  2) Starte mit vorhandener Datenbank")
     print("  3) Abbrechen")
 
