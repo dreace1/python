d23d526385223e9cab9135ec41d2fdf75de36d3d
Hannes <hannesro27@gmail.com>
2023-01-12 08:31:25 +0100

    implmented click method

--- cookie_bot_task/Vorlage_Firefox/cookie_clicker_bot_Vorlage.py
+++ cookie_bot_task/Vorlage_Firefox/cookie_clicker_bot_Vorlage.py
@@ -1,4 +1,8 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service  
+import os
+from time import sleep
 """
 ToDo: Welche Bibliotheken müssen eingebunden werden?
 Lesen Sie hierzu IB-Selenium Webdriver
@@ -10,13 +14,9 @@ class cookieBot():
 
     def __init__(self):
 
-        #s = Service('./geckodriver') #Linux und Mac
-        s = Service('geckodriver.exe') #Windows
-
-        options = Options()
-        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-        self.browser = webdriver.Firefox(executable_path=r'C:\WebDrivers\geckodriver.exe', options=options)
-       
+        s = Service('./geckodriver') #Linux und Mac
+        self.browser = webdriver.Firefox(service=s) 
+    
         # Kopieren Sie den Source Code für CookieClicker aus dem Moodle-Kurs in dieses Verzeichnis
         directory = os.path.realpath(os.path.dirname(__file__)) # diese OS-Methode gibt Dateipfad zurück
         path = 'file://'+ directory + '/CookieClickerSource/index.html' #Dateipfad zum Spiel auf Ihrem PC
@@ -26,8 +26,16 @@ class cookieBot():
         -- Ab hier ist die Webseite geladen --
         ToDo: Legen Sie geeignete Variablen an
         """
+    def clickCookie(self):
+        success = False
+        element = self.browser.find_element(By.ID,'bigCookie') 
+        element.click()
+        success = True
+        
+        return success
    
 bot = cookieBot()
-time.sleep(1)
+sleep(1)
+
 while(True):
     bot.clickCookie()
