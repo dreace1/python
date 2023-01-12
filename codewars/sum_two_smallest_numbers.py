1162a6e2f78d8e926deefa4987962d54e49d8eb4
Hannes <hannesro27@gmail.com>
2023-01-12 08:59:15 +0100

    finished sum_two_smalles_numbers kata

---
+++ codewars/sum_two_smallest_numbers.py
@@ -0,0 +1,4 @@
+def sum_two_smallest_numbers(numbers:list):
+    numbers.sort()
+    return numbers[0] + numbers [1]
+
--- codewars/tower_build.py
+++
@@ -1,5 +0,0 @@
-def tower_builder(n_floors):
-    output_list = []
-    for i in n_floors:
-        output += "*"
-        
