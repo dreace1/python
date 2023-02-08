5606e6ab4ae6b224311ef41e433bf18920479bea
Hannes <hannesro27@gmail.com>
2023-02-08 11:52:15 +0100

    implemented labels for the textfields

--- bmi_calculator/bmi_view.py
+++ bmi_calculator/bmi_view.py
@@ -10,25 +10,30 @@ class Bmi_view(customtkinter.CTk):
         super().__init__()
         self.geometry("400x240")
         
+        self.init_grid_laylout()
         self.init_input_fields()
+        self.init_labels()
         
     def init_grid_laylout(self):
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)        
+        self.grid_rowconfigure(0, weight = 0)
+        self.grid_columnconfigure((0, 1), weight = 0)    
+          
 
     
         
     def init_input_fields(self): 
         weight_input_field = customtkinter.CTkEntry(master = self, placeholder_text="in kg")
-        weight_input_field.grid(row = 1, column = 2, padx = 20, pady = 20)
+        weight_input_field.grid(column = 1, row = 0, padx = 20, pady = 20)
         
         height_input_field = customtkinter.CTkEntry(master = self, placeholder_text="in m")
-        height_input_field.grid(row = 2, column = 2, padx = 20, pady = 20)
+        height_input_field.grid(column = 1, row = 1, padx = 20, pady = 20)
 
     def init_labels(self):
-        weight_label = customtkinter.CTkLabel(self, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        weight_label = customtkinter.CTkLabel(self, text="Gewicht", font=customtkinter.CTkFont(family = "futura", size=20))
+        weight_label.grid(column = 0, row = 0, padx = 20, pady = 20)
 
+        height_label = customtkinter.CTkLabel(self, text="Höhe", font=customtkinter.CTkFont(family = "futura", size=20))
+        height_label.grid(column = 0, row = 1, padx = 20, pady = 20)
     
 view = Bmi_view()
 view.mainloop()
