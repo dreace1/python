0277ac1d0eb62222af5d2f90c28ad637f6a91b28
Hannes <hannesro27@gmail.com>
2023-02-09 08:09:31 +0100

    Added grid layout and the labels and buttons for the bmi calc

--- bmi_calculator/bmi_view.py
+++ bmi_calculator/bmi_view.py
@@ -8,25 +8,24 @@ class Bmi_view(customtkinter.CTk):
 
     def __init__(self) -> None:
         super().__init__()
-        self.geometry("400x240")
+        self.geometry("350x300")
         
         self.init_grid_laylout()
         self.init_input_fields()
         self.init_labels()
+        self.init_calculate_button()
+        self.init_bmi_label()
         
     def init_grid_laylout(self):
         self.grid_rowconfigure(0, weight = 0)
         self.grid_columnconfigure((0, 1), weight = 0)    
-          
-
-    
         
     def init_input_fields(self): 
-        weight_input_field = customtkinter.CTkEntry(master = self, placeholder_text="in kg")
-        weight_input_field.grid(column = 1, row = 0, padx = 20, pady = 20)
+        self.weight_input_field = customtkinter.CTkEntry(master = self, placeholder_text="in kg")
+        self.weight_input_field.grid(column = 1, row = 0, padx = 20, pady = 20)
         
-        height_input_field = customtkinter.CTkEntry(master = self, placeholder_text="in m")
-        height_input_field.grid(column = 1, row = 1, padx = 20, pady = 20)
+        self.height_input_field = customtkinter.CTkEntry(master = self, placeholder_text="in m")
+        self.height_input_field.grid(column = 1, row = 1, padx = 20, pady = 20)
 
     def init_labels(self):
         weight_label = customtkinter.CTkLabel(self, text="Gewicht", font=customtkinter.CTkFont(family = "futura", size=20))
@@ -34,6 +33,19 @@ class Bmi_view(customtkinter.CTk):
 
         height_label = customtkinter.CTkLabel(self, text="Höhe", font=customtkinter.CTkFont(family = "futura", size=20))
         height_label.grid(column = 0, row = 1, padx = 20, pady = 20)
-    
+
+    def init_calculate_button(self):
+        self.calculate_button = customtkinter.CTkButton(self, text = "Berechnen", width = 300, command = self.calculate_event)
+        self.calculate_button.grid(column = 0, row = 2, columnspan = 2, padx = 20, pady = 20)
+
+    def init_bmi_label(self):
+        self.bmi_label = customtkinter.CTkLabel(self, fg_color = "#a6d189", text_color = "black", text = "BMI-Wert: 0", 
+                                           font = customtkinter.CTkFont(family = "futura" ,size = 20))
+        self.bmi_label.grid(column = 0, row = 3, columnspan = 2, padx = 20, pady = 20)
+
+    def calculate_event(self):
+        bmi = float(self.weight_input_field.get()) / pow(float(self.height_input_field.get()), 2)
+        self.bmi_label.configure(text = "BMI-Wert: " + str(round(bmi, 2)))
+
 view = Bmi_view()
 view.mainloop()
